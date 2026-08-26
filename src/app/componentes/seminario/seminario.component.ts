bd2a4db75767d8615cfd9ea6d240821bd1b8e56c
eudermes <eudermes11@gmail.com>
2026-08-26 18:26:22 -0300

    aba resumo seminario

--- src/app/componentes/seminario/seminario.component.ts
+++ src/app/componentes/seminario/seminario.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-seminario',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './seminario.component.html',
   styleUrl: './seminario.component.css'
 })
 export class SeminarioComponent {
+  // Controle de Abas Internas
+  abaAtiva: string = 'resumo';
 
+  // Dados reais extraídos da imagem do Seminário
+  resumoSeminario = {
+    totalJovensAla: 53,
+    totalMatriculados: { valor: 9, pct: 17 },
+    frequenciaAcima75: { valor: 2, pct: 22 },
+    frequenciaAbaixo75: { valor: 7, pct: 78 },
+    frequenciaMediaTurma: 41
+  };
+
+  mudarAba(nomeDaAba: string): void {
+    this.abaAtiva = nomeDaAba;
+  }
 }
